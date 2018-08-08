4ce9ffc84229399c1c146e3dcf930b12921d923c
Michael Bryne <mbryne@gmail.com>
2018-08-08 21:52:49 +1000

    Updated Hardware

--- mSynth/test/test.cpp
+++ mSynth/test/test.cpp
@@ -12,47 +12,79 @@
 //////////////////////////////////
 
 //
-//  POTS SETUP
-//
-
-const int POT_1 = A14;
-const int POT_2 = A15;
-const int POT_3 = A16;
-const int POT_4 = A17;
-const int POT_5 = A18;
-const int POT_6 = A19;
-
-ResponsiveAnalogRead pot1(POT_1, true);
-ResponsiveAnalogRead pot2(POT_2, true);
-ResponsiveAnalogRead pot3(POT_3, true);
-ResponsiveAnalogRead pot4(POT_4, true);
-ResponsiveAnalogRead pot5(POT_5, true);
-ResponsiveAnalogRead pot6(POT_6, true);
-
-//
 //  BUTTON SETUP
 //
 
-const int BUTTON_1 = 31;
+const int BUTTON_1 = 33;
 const int BUTTON_2 = 32;
 
-Bounce button1 = Bounce(BUTTON_1, 25);
-Bounce button2 = Bounce(BUTTON_2, 25);
+Bounce button1 = Bounce(BUTTON_1, 50);
+Bounce button2 = Bounce(BUTTON_2, 50);
 
 //
 //  ROTARY SETUP
 //
 
-const int ROTARY_BUTTON_1 = 24;
-const int ROTARY_DATA_1 = 25;
-const int ROTARY_DATA_2 = 26;
+const int ROTARY_COUNT = 7;
+const int ROTARY_0_BUTTON = 2;
+const int ROTARY_0_DATA_1 = 24;
+const int ROTARY_0_DATA_2 = 25;
+const int ROTARY_1_BUTTON = 3;
+const int ROTARY_1_DATA_1 = 27;
+const int ROTARY_1_DATA_2 = 26;
+const int ROTARY_2_BUTTON = 4;
+const int ROTARY_2_DATA_1 = 29;
+const int ROTARY_2_DATA_2 = 28;
+const int ROTARY_3_BUTTON = 5;
+const int ROTARY_3_DATA_1 = 31;
+const int ROTARY_3_DATA_2 = 30;
+const int ROTARY_4_BUTTON = 6;
+const int ROTARY_4_DATA_1 = 35;
+const int ROTARY_4_DATA_2 = 34;
+const int ROTARY_5_BUTTON = 7;
+const int ROTARY_5_DATA_1 = 37;
+const int ROTARY_5_DATA_2 = 36;
+const int ROTARY_6_BUTTON = 8;
+const int ROTARY_6_DATA_1 = 39;
+const int ROTARY_6_DATA_2 = 38;
 
-Bounce rotaryButton = Bounce(ROTARY_BUTTON_1, 25);
+struct KnobControl {
+  Encoder encoder;
+  Bounce button;
+	int newPosition = 0;
+  int currentPosition = -999;
+};
 
-Encoder rotary(ROTARY_DATA_1, ROTARY_DATA_2);
-
-long newPosition;
-long rotaryPosition  = -999;
+struct KnobControl Controls[ROTARY_COUNT] = {
+    {
+      Encoder(ROTARY_0_DATA_1, ROTARY_0_DATA_2),
+      Bounce(ROTARY_0_BUTTON, 50)
+    },
+    {
+      Encoder(ROTARY_1_DATA_1, ROTARY_1_DATA_2),
+      Bounce(ROTARY_1_BUTTON, 50)
+    },
+    {
+      Encoder(ROTARY_2_DATA_1, ROTARY_2_DATA_2),
+      Bounce(ROTARY_2_BUTTON, 50)
+    },
+    {
+      Encoder(ROTARY_3_DATA_1, ROTARY_3_DATA_2),
+      Bounce(ROTARY_3_BUTTON, 50)
+    },
+    {
+      Encoder(ROTARY_4_DATA_1, ROTARY_4_DATA_2),
+      Bounce(ROTARY_4_BUTTON, 50)
+    },
+    {
+      Encoder(ROTARY_5_DATA_1, ROTARY_5_DATA_2),
+      Bounce(ROTARY_5_BUTTON, 50)
+    },
+    {
+      Encoder(ROTARY_6_DATA_1, ROTARY_6_DATA_2),
+      Bounce(ROTARY_6_BUTTON, 50)
+    }
+  };
 
 //
 //  DISPLAY SETUP
@@ -80,7 +112,13 @@ void setup() {
   //  ROTARY SETUP
   //
 
-  pinMode(ROTARY_BUTTON_1, INPUT_PULLUP);
+  pinMode(ROTARY_0_BUTTON, INPUT_PULLUP);
+  pinMode(ROTARY_1_BUTTON, INPUT_PULLUP);
+  pinMode(ROTARY_2_BUTTON, INPUT_PULLUP);
+  pinMode(ROTARY_3_BUTTON, INPUT_PULLUP);
+  pinMode(ROTARY_4_BUTTON, INPUT_PULLUP);
+  pinMode(ROTARY_5_BUTTON, INPUT_PULLUP);
+  pinMode(ROTARY_6_BUTTON, INPUT_PULLUP);
 
   //
   //  LCD SETUP
@@ -92,8 +130,8 @@ void setup() {
   lcd.backlight();
   lcd.setCursor(3,0);// Set the cursor at line 1, position 3. The second parameter '0' represent line 1.
   lcd.print("Hello, world!");
-  lcd.setCursor(1,1);
-  lcd.print("mSynth is Live!");// Set the cursor at line 2, position 2.The second parameter '1' represent line 2.
+  lcd.setCursor(0,1);
+  lcd.print("mSynth2 is Live!");// Set the cursor at line 2, position 2.The second parameter '1' represent line 2.
 
 }
 
@@ -111,46 +149,15 @@ void loop() {
   button1.update();
   button2.update();
 
-  //  update our pots bounce
-  pot1.update();
-  pot2.update();
-  pot3.update();
-  pot4.update();
-  pot5.update();
-  pot6.update();
-
-  //  update our rotary bounce
-  rotaryButton.update();
+  //  update our rotary buttons bounce
+  for( int i = 0; i < ROTARY_COUNT; i++) {
+    Controls[i].button.update();
+  }
 
   //////////////////////////////////
   //  TESTING
   //////////////////////////////////
 
-  //  pots test
-  if(pot1.hasChanged()) {
-    Serial.print("pot1: ");
-    Serial.println(pot1.getValue() / 4);
-  }
-  if(pot2.hasChanged()) {
-    Serial.print("pot2: ");
-    Serial.println(pot2.getValue() / 4);
-  }
-  if(pot3.hasChanged()) {
-    Serial.print("pot3: ");
-    Serial.println(pot3.getValue() / 4);
-  }
-  if(pot4.hasChanged()) {
-    Serial.print("pot4: ");
-    Serial.println(pot4.getValue() / 4);
-  }
-  if(pot5.hasChanged()) {
-    Serial.print("pot5: ");
-    Serial.println(pot5.getValue() / 4);
-  }
-  if(pot6.hasChanged()) {
-    Serial.print("pot6: ");
-    Serial.println(pot6.getValue() / 4);
-  }
 
   //  buttons test
   if (button1.fallingEdge()) {
@@ -160,18 +167,26 @@ void loop() {
     Serial.println("button2: pushed");
   }
 
-  //  rotary position test
-  newPosition = rotary.read();
-  if (newPosition != rotaryPosition) {
-    Serial.print("rotary: ");
-    Serial.println(newPosition);
-    rotaryPosition = newPosition;
-  }
+  //  update our rotary buttons bounce
+  for( int i = 0; i < ROTARY_COUNT; i++) {
 
-  //  rotary buttons test
-  if (rotaryButton.fallingEdge()) {
-    Serial.println("rotaryButton: pushed");
-  }
+    //  rotary position
+    Controls[i].newPosition = Controls[i].encoder.read();
+    if (Controls[i].newPosition != Controls[i].currentPosition) {
+      Serial.print("rotary ");
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.println(Controls[i].newPosition);
+      Controls[i].currentPosition = Controls[i].newPosition;
+    }
 
+    //  button position
+    if (Controls[i].button.fallingEdge()) {
+      Serial.print("rotaryButton ");
+      Serial.print(i);
+      Serial.println(": pushed ");
+    }
+
+  }
 
 }
